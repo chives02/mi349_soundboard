6c81daa7765fcd430a006999c9880ed649eca317
Chives <danielrighter1@gmail.com>
2021-11-18 22:21:55 -0500

    chives

---
+++ soundboard.js
@@ -0,0 +1,26 @@
+const sounds = ["wow 😍", "hooray 😄", "victory😆"];
+
+sounds.forEach((sound) => {
+  const btn = document.createElement("button");
+  btn.classList.add("btn");
+
+  btn.innerText = sound;
+
+  btn.addEventListener("click", () => {
+    stopSongs();
+
+    document.getElementById(sound).play();
+  });
+
+  document.getElementById("buttons").appendChild(btn);
+});
+
+function stopSongs() {
+    sounds.forEach((sound) => {
+      const song = document.getElementById(sound);
+  
+      song.pause();
+      song.currentTime = 0;
+    });
+  }
+
